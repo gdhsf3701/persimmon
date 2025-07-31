4022415265093d94121e5afff481f67d48c00e7d
moon080411 <anstjdalsdlek@gmail.com>
2025-08-01 01:22:37 +0900

    fix:EnemySpawnManager

--- Assets/moon/01.Script/Manager/GameManager.cs
+++ Assets/moon/01.Script/Manager/GameManager.cs
@@ -14,12 +14,21 @@ namespace moon._01.Script.Manager
         [SerializeField] private ScriptFinderSO spawnManagerFinder;
         [SerializeField] private ScriptFinderSO scoreManagerFinder;
 
-        [field: SerializeField ,SerializedDictionary("Wave", "EnemyList")]
-        public SerializedDictionary<int, WaveDataListSO> waveEnemy {get; private set;}
+        [field: SerializeField]
+        public List<WaveDataListSO> WaveEnemy {get; private set;}
+        
+        [field: SerializeField]
+        public WaveDataListSO BossEnemy {get; private set;}
         public EnemySpawnManager SpawnManager { get; private set; }
         public ScoreManager ScoreManager { get; private set; }
 
-        public Action OnGameEndEvent;
+        public bool AllKillBoss { get; private set; } = false;
+
+        public Action OnCutSceneEnd;
+
+        public event Action OnBossScene;
+
+        public event Action OnGameEndEvent;
 
         private void Awake()
         {
@@ -27,17 +36,32 @@ namespace moon._01.Script.Manager
             ScoreManager = scoreManagerFinder.GetTarget<ScoreManager>();
             ScoreManager.Initialize(this);
             SpawnManager.NextWaveEvent += NextWave;
+            OnCutSceneEnd += SetNextWave;
             ResetWave();
         }
 
         private void OnDestroy()
         {
             SpawnManager.NextWaveEvent -= NextWave;
+            OnCutSceneEnd -= SetNextWave;
         }
 
         public void NextWave()
         {
             Wave++;
+            if (!AllKillBoss && Wave <= WaveEnemy.Count)
+            {
+                var (spawnCount,spawnTime, enemyPrefabs) = GetWaveData();
+                SpawnManager.SetNextWave(spawnCount,spawnTime,enemyPrefabs);
+            }
+            else
+            {
+                OnBossScene?.Invoke();
+            }
+        }
+
+        public void SetNextWave()
+        {
             var (spawnCount,spawnTime, enemyPrefabs) = GetWaveData();
             SpawnManager.SetNextWave(spawnCount,spawnTime,enemyPrefabs);
         }
@@ -48,15 +72,23 @@ namespace moon._01.Script.Manager
             var (spawnCount,spawnTime, enemyPrefabs) = GetWaveData();
             SpawnManager.ResetSpawnManager(spawnCount,spawnTime,enemyPrefabs);
             ScoreManager.ResetScoreManager();
+            AllKillBoss = false;
         }
 
         public (int,float,List<GameObject>) GetWaveData()
         {
-            if (waveEnemy.ContainsKey(Wave))
+            if (Wave <= WaveEnemy.Count)
             {
-                WaveDataListSO waveDataList = waveEnemy[Wave];
+                WaveDataListSO waveDataList = WaveEnemy[Wave - 1];
                 return (waveDataList.EnemySpawnCount, waveDataList.SpawnTime ,waveDataList.EnemyPrefabs);
             }
+
+            if (!AllKillBoss)
+            {
+                AllKillBoss = true;
+                return (BossEnemy.EnemySpawnCount, BossEnemy.SpawnTime ,BossEnemy.EnemyPrefabs);
+            }
+
             OnGameEndEvent?.Invoke();
             return (0, 0, new List<GameObject>());
         }
