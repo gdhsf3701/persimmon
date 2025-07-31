bd187a42bee6a475c4cc82c17fee93086b300413
박준서 <pppg12jintpt@gmail.com>
2025-07-31 13:47:49 +0900

    feat:enemy Position Set

--- Assets/Work/Bakbak/Code/EnemyMover.cs
+++ Assets/Work/Bakbak/Code/EnemyMover.cs
@@ -4,19 +4,21 @@ public class EnemyMover : MonoBehaviour, IEntityCompo
 {
     private Enemy owner;
     private Vector2 moveDir = Vector2.zero;
+    [SerializeField]
     private float moveSpeed;
     void Update()
     {
         transform.position += (Vector3) moveDir * moveSpeed * Time.deltaTime;
     }
 
-    public void SetDir(Vector2 dir)
+    public void SetTargetPos(Vector2 pos)
     {
-        moveDir = dir;
+        moveDir = (pos -(Vector2)transform.position).normalized;
     }
 
     public void Initialize(Enemy enemy)
     {
         owner = enemy;
+        SetTargetPos(new Vector2(0, 0));
     }
 }
