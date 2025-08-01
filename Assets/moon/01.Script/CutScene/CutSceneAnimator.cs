25d0a2c20590b9e4f7f66fd7a33b488c3ef05882
moon080411 <anstjdalsdlek@gmail.com>
2025-08-01 12:40:57 +0900

    feat:CutScene Animator

---
+++ Assets/moon/01.Script/CutScene/CutSceneAnimator.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace moon._01.Script.CutScene
+{
+    public class CutSceneAnimator : MonoBehaviour
+    {
+        public event Action OnAnimationEndEvent;
+        [SerializeField]private Animator animator;
+        private string _currentAnimationName;
+
+        public void PlayAnimator(string name)
+        {
+            if(!string.IsNullOrEmpty(_currentAnimationName))
+                animator.SetBool(_currentAnimationName, false);
+            animator.SetBool(name, true);
+            _currentAnimationName = name;
+        }
+        
+        public void EndAnimation()
+        {
+            if(!string.IsNullOrEmpty(_currentAnimationName))
+                animator.SetBool(_currentAnimationName, false);
+            _currentAnimationName = null;
+            OnAnimationEndEvent?.Invoke();
+        }
+    }
+}
