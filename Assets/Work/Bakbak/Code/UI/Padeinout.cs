7a64279af728d8bf2357aeb1fa52483e6de84ff9
박준서 <pppg12jintpt@gmail.com>
2025-08-01 11:47:29 +0900

    feat:jellyFish, SoftTutle

--- Assets/Work/Bakbak/Code/UI/Padeinout.cs
+++ Assets/Work/Bakbak/Code/UI/Padeinout.cs
@@ -1,6 +1,24 @@
+using DG.Tweening;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Padeinout : MonoBehaviour
 {
-    
+    [SerializeField]    
+    private Image padePanel;
+    [SerializeField]
+    private float fadetime;
+    private readonly int _valueHash = Shader.PropertyToID("_value");
+    [ContextMenu("fin")]
+    public void PadeIn()
+    {
+        padePanel.material.SetFloat(_valueHash, 0);
+        padePanel.material.DOFloat(5,_valueHash, fadetime).SetEase(Ease.InQuad);
+    }
+    [ContextMenu("fout")]
+    public void PadeOut()
+    {
+        padePanel.material.SetFloat(_valueHash, 5);
+        padePanel.material.DOFloat(0, _valueHash, fadetime).SetEase(Ease.OutQuad);
+    }
 }
